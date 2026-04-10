f29da58e250407fc4792e65774f91129b55b3fb4
berrethenterprises-jpg <berrethenterprises@gmail.com>
2026-04-09 19:27:00 -0600

    Update walletManager.js

--- modules/walletManager.js
+++ modules/walletManager.js
@@ -1,14 +1,14 @@
 const wallets = [
-  "DWVEEjuGNTuR1zDxcgoAQPvfTpxxcgpCjqrwwCiZzEeV,"
+  "DWVEEjuGNTuR1zDxcgoAQPvfTpxxcgpCjqrwwCiZzEeV",
   "EKpspUNsUhf6u2uXVbqLENuMhg8dqjGUisKpmrDWrmjq",
-  "WALLET_ADDRESS_3",
-  "WALLET_ADDRESS_4",
-  "WALLET_ADDRESS_5",
-  "WALLET_ADDRESS_6",
-  "WALLET_ADDRESS_7",
-  "WALLET_ADDRESS_8",
-  "WALLET_ADDRESS_9",
-  "WALLET_ADDRESS_10"
+  "6SqEKo9y7tJoBJenNupp82LQn87br4khstzLQF65tY5a",
+  "HoZoEzUBf5omdUdGHL7tDgeqasD4WFFPKTYfFSMQh1Eg",
+  "54Pz1e35z9uoFdnxtzjp7xZQoFiofqhdayQWBMN7dsuy",
+  "6wcTgQBJmnRdQWDbrLFgk45JSW7LQ9cej3TtcFCvrape",
+  "CUwyqt27UtTBxyxvj1hPqqyLVzrUgcDWAPJvwqnnwahn",
+  "46kFyRMYjZFgEgR83Mi5oaD7TQwUn8qnspoPG8HyvYqA",
+  "12VFrc1dFynPzs4HBRdRoMKNm1jca2S2uhaNKCkdffwy",
+  "BaJryu4sRRqPrZLWbPy7fZw14MBTyChySNG71MhQvGcw"
 ];
 
 function getWallets() {
